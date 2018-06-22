6bbf0c82c1ea09bd6e87bb3366d0f0266c9bfbc6
m-matosflorimon <m.matosflorimon@gmail.com>
2018-06-21 23:56:35 -0400

    committing

--- SteticTutorial/SteticTutorial/SteticTutorial/MainWindow.cs
+++ SteticTutorial/SteticTutorial/SteticTutorial/MainWindow.cs
@@ -13,4 +13,11 @@ public partial class MainWindow : Gtk.Window
         Application.Quit();
         a.RetVal = true;
     }
+
+	protected void OnOpen(object sender, EventArgs e)
+	{
+		Application.Quit();
+	}
+
+
 }
